0dc63ce3a542ed75a1cce48a19b18548ed041d78
patrick burskey <pburskey@msn.com>
2020-12-08 08:28:09 -0600

    day 4 part two

--- day4/doit.go
+++ day4/doit.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -44,7 +45,7 @@ func (p *passport) validateRange(aString string, low int, high int) bool {
 	valid := false
 	var aNumber int
 	aNumber, err := strconv.Atoi(aString)
-	if err != nil {
+	if err == nil {
 		valid = true
 		valid = valid && aNumber >= low
 		valid = valid && aNumber <= high
@@ -54,31 +55,78 @@ func (p *passport) validateRange(aString string, low int, high int) bool {
 
 func (p *passport) validateHeight(aString string) bool {
 	valid := false
-	var aNumber int
-	aNumber, err := strconv.Atoi(aString)
-	if err != nil {
-		valid = true
-		valid = valid && aNumber >= low
-		valid = valid && aNumber <= high
+	//60in
+	//190cm
+	if strings.Contains(aString, "in") {
+		aStringPortion := strings.ReplaceAll(aString, "in", "")
+		aNumber, err := strconv.Atoi(aStringPortion)
+		if err == nil {
+
+			if aNumber >= 59 && aNumber <= 76 {
+				valid = true
+			}
+		}
+	} else if strings.Contains(aString, "cm") {
+		aStringPortion := strings.ReplaceAll(aString, "cm", "")
+		aNumber, err := strconv.Atoi(aStringPortion)
+		if err == nil {
+
+			if aNumber >= 150 && aNumber <= 193 {
+				valid = true
+			}
+		}
 	}
+
 	return valid
 }
 
 func (p *passport) validate() bool {
 
-	v := func(aString string) bool {
-		return len(aString) > 0
-	}
+	//v := func(aString string) bool {
+	//	return len(aString) > 0
+	//}
 	var valid bool
 	valid = true
 	valid = valid && p.validateRange(p.birthYear, 1920, 2002)
 	valid = valid && p.validateRange(p.issueYear, 2010, 2020)
 	valid = valid && p.validateRange(p.expirationYear, 2020, 2030)
-	valid = valid && v(p.height)
-	valid = valid && v(p.hairColor)
-	valid = valid && v(p.eyeColor)
-	valid = valid && v(p.passportID)
+	valid = valid && p.validateHeight(p.height)
+	valid = valid && p.validateHairColor(p.hairColor)
+	valid = valid && p.validateEyeColor(p.eyeColor)
+	valid = valid && p.validatePassportID(p.passportID)
 	//valid = v(p.countryID)
+	return valid
+}
+
+func (p *passport) validateEyeColor(aString string) bool {
+	valid := false
+
+	pattern := "^(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)$"
+
+	valid, err := regexp.MatchString(pattern, aString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return valid
+}
+
+func (p *passport) validateHairColor(aString string) bool {
+	valid := false
+	pattern := "^#[a-fA-F0-9]{6}$"
+	valid, err := regexp.MatchString(pattern, aString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return valid
+}
+
+func (p *passport) validatePassportID(aString string) bool {
+	valid := false
+	pattern := "^[0-9]{9}$"
+	valid, err := regexp.MatchString(pattern, aString)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return valid
 }
 
