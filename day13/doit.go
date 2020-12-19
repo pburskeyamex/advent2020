c402519ee9e74a56a38c460bb6ff16bf61be841d
patrick burskey <pburskey@msn.com>
2020-12-18 22:17:18 -0600

    starting day 13

--- day13/doit.go
+++ day13/doit.go
@@ -6,194 +6,48 @@ import (
 	"log"
 	"os"
 	"strconv"
-	strings2 "strings"
+	"strings"
 )
+
+type bus struct {
+	id    int
+	times []int
+}
 
 func main() {
 
-	//newX, xDirection, newY, yDirection := rotateRight(360, 10, EAST, 4, NORTH)
-	//log.Print(newX)
-	//log.Print(xDirection)
-	//log.Print(newY)
-	//log.Print(yDirection)
-	//
-	//newX, xDirection, newY, yDirection = rotateLeft(360, 10, EAST, 4, NORTH)
-	//log.Print(newX)
-	//log.Print(xDirection)
-	//log.Print(newY)
-	//log.Print(yDirection)
+	data := Parse("day_13_sample_data.txt")
+	data1 := data[0]
+	data2 := data[1]
 
-	//proof("day_11_sample_data_2.txt")
+	goalTimeToLeave, _ := strconv.Atoi(data1)
+	log.Printf("%d\n", goalTimeToLeave)
+	busses := make([]*bus, 0)
 
-	data := Parse("day_12_data.txt")
+	for _, aString := range strings.Split(data2, ",") {
+		if aString != "x" {
+			id, _ := strconv.Atoi(aString)
+			bus := &bus{
+				id:    id,
+				times: make([]int, 0),
+			}
+			bus.times = append(bus.times, 0)
+			bus.times = append(bus.times, id)
+			busses = append(busses, bus)
+		}
+	}
+
+	for i := 0; i < len(busses); i++ {
+		bus := busses[i]
+
+		for x := goalTimeToLeave / bus.id; x < (goalTimeToLeave * (bus.id + 1)); x++ {
+			aTime := x
+			bus.times = append(bus.times, aTime)
+		}
+	}
+
+	log.Printf(data2)
 	//data := Parse("day_12_data.txt")
-
-	movements := make([]*movement, 0)
-
-	for _, aString := range data {
-		aMovement := parse(aString)
-		movements = append(movements, aMovement)
-	}
-
-	positions := make([]*vessel, 0)
-
-	startingPosition := &vessel{
-		orientation: EAST,
-		position: &ddPoint{
-			x: &compassValue{
-				direction: EAST,
-				value:     0,
-			},
-			y: &compassValue{
-				direction: NORTH,
-				value:     0,
-			},
-		},
-		waypoint: &ddPoint{
-			x: &compassValue{
-				direction: EAST,
-				value:     10,
-			},
-			y: &compassValue{
-				direction: NORTH,
-				value:     1,
-			},
-		},
-	}
-	positions = append(positions, startingPosition)
-
-	for i := 0; i < len(movements); i++ {
-
-		aMovement := movements[i]
-
-		last := lastPosition(positions)
-
-		nextPosition := &vessel{
-			orientation: last.orientation,
-			position: &ddPoint{
-				x: &compassValue{
-					direction: last.position.x.direction,
-					value:     last.position.x.value,
-				},
-				y: &compassValue{
-					direction: last.position.y.direction,
-					value:     last.position.y.value,
-				},
-			},
-			waypoint: &ddPoint{
-				x: &compassValue{
-					direction: last.waypoint.x.direction,
-					value:     last.waypoint.x.value,
-				},
-				y: &compassValue{
-					direction: last.waypoint.y.direction,
-					value:     last.waypoint.y.value,
-				},
-			},
-		}
-		positions = append(positions, startingPosition)
-
-		if aMovement.direction.value == "F" {
-
-			/*
-				plot where we are starting....
-			*/
-			startingX := nextPosition.position.x.value
-			if nextPosition.position.x.direction == WEST {
-				startingX = startingX * -1
-			}
-			startingY := nextPosition.position.y.value
-			if nextPosition.position.y.direction == SOUTH {
-				startingY = startingY * -1
-			}
-
-			/*
-				find the adjustment to the position by moving to the waypoint n times.....
-			*/
-			wayPoint := nextPosition.waypoint
-			wayPointAdjustmentX := 0
-			wayPointAdjustmentY := 0
-			for i := 0; i < aMovement.value; i++ {
-				wayPointAdjustmentX += (wayPoint.x.value)
-				wayPointAdjustmentY += (wayPoint.y.value)
-			}
-
-			if wayPoint.x.direction == WEST {
-				wayPointAdjustmentX = wayPointAdjustmentX * -1
-			}
-			if wayPoint.y.direction == SOUTH {
-				wayPointAdjustmentY = wayPointAdjustmentY * -1
-			}
-
-			nextPosition.position.x.value = (startingX + wayPointAdjustmentX)
-			nextPosition.position.y.value = (startingY + wayPointAdjustmentY)
-
-			if nextPosition.position.x.value < 0 {
-				nextPosition.position.x.value *= -1
-				nextPosition.position.x.direction = WEST
-			} else {
-				nextPosition.position.x.direction = EAST
-			}
-
-			if nextPosition.position.y.value < 0 {
-				nextPosition.position.y.value *= -1
-				nextPosition.position.y.direction = SOUTH
-			} else {
-				nextPosition.position.y.direction = NORTH
-			}
-
-		} else if aMovement.direction.value == "E" {
-			adjustedValue, adjustedDirection := addX(nextPosition.waypoint.x.value, nextPosition.waypoint.x.direction, aMovement.value, EAST)
-			nextPosition.waypoint.x.value = adjustedValue
-			nextPosition.waypoint.x.direction = adjustedDirection
-		} else if aMovement.direction.value == "W" {
-			adjustedValue, adjustedDirection := addX(nextPosition.waypoint.x.value, nextPosition.waypoint.x.direction, aMovement.value, WEST)
-			nextPosition.waypoint.x.value = adjustedValue
-			nextPosition.waypoint.x.direction = adjustedDirection
-		} else if aMovement.direction.value == "S" {
-			adjustedValue, adjustedDirection := addY(nextPosition.waypoint.y.value, nextPosition.waypoint.y.direction, aMovement.value, SOUTH)
-			nextPosition.waypoint.y.value = adjustedValue
-			nextPosition.waypoint.y.direction = adjustedDirection
-		} else if aMovement.direction.value == "N" {
-			adjustedValue, adjustedDirection := addY(nextPosition.waypoint.y.value, nextPosition.waypoint.y.direction, aMovement.value, NORTH)
-			nextPosition.waypoint.y.value = adjustedValue
-			nextPosition.waypoint.y.direction = adjustedDirection
-		} else if aMovement.direction.value == "R" {
-
-			newX, xDirection, newY, yDirection := rotateRight(aMovement.value, nextPosition.waypoint.x.value, nextPosition.waypoint.x.direction, nextPosition.waypoint.y.value, nextPosition.waypoint.y.direction)
-			nextPosition.waypoint.x.direction = xDirection
-			nextPosition.waypoint.x.value = newX
-
-			nextPosition.waypoint.y.direction = yDirection
-			nextPosition.waypoint.y.value = newY
-
-		} else if aMovement.direction.value == "L" {
-
-			newX, xDirection, newY, yDirection := rotateLeft(aMovement.value, nextPosition.waypoint.x.value, nextPosition.waypoint.x.direction, nextPosition.waypoint.y.value, nextPosition.waypoint.y.direction)
-			nextPosition.waypoint.x.direction = xDirection
-			nextPosition.waypoint.x.value = newX
-
-			nextPosition.waypoint.y.direction = yDirection
-			nextPosition.waypoint.y.value = newY
-
-		} else {
-			log.Panicf("Direction: %s not understood", aMovement.direction.value)
-		}
-
-		nextPosition.manhattanDistance = &manhattanDistance{}
-		nextPosition.manhattanDistance.collect(nextPosition)
-
-		positions = append(positions, nextPosition)
-
-	}
-
-	aManhattanDistance := &manhattanDistance{
-		x: 0,
-		y: 0,
-	}
-	aManhattanDistance.collectAll(positions)
-
-	fmt.Println(fmt.Sprintf("Manhattan Distance... East / West: %d North / South: %d .... Total: %d", aManhattanDistance.x, aManhattanDistance.y, aManhattanDistance.distance()))
 
 }
 
